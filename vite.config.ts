cc0d413ce79f37dca664da3d07887c5a4ed90a97
chenyakun002 <chenyakun002@lianjia.com>
2025-03-18 11:31:52 +0800

    修改deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/plan-manager/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/week-plan/' : '/',
   server: {
     port: 9528,
   },
